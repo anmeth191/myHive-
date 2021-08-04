05a887fe8c91ed01695ddaa5a47ccb9cb312f865
Angel Rivera <38502560+anmeth191@users.noreply.github.com>
2021-08-03 22:09:11 -0400

    Add files via upload
    created the new state with all the information i need to send to the server and then save to the data base

--- AddProduct.js
+++ AddProduct.js
@@ -7,86 +7,144 @@ constructor(props){
 super(props)
 
   this.state = {
-      categories:[],
-      suppliers:[],
-      image:[],
-      photo:''
+     categories:[],//this array dipslay in the select tag the data is coming from the database 
+     suppliers:[],//this array displays in the select tag the suppliers coming from the database
+     product_name:'', //product name that is going to be saved
+     product_description:'',
+     product_price:'',
+     product_unit:'',
+     product_quantity:'',
+     product_category:'',
+     product_supplier:'',
+     product_photo:[]
+     
   }
 }
 
+//this method makes a request to the API and gets the data from the server then set the state and is rendered in the select tag in options
 getCategories = async ()=>{
 await axios.get('http://127.0.0.1:8000/categories').then( response =>{
 this.setState({ categories:response.data.body} , ()=>{ return this.state.categories})
 })
 }
 
-
+//this method makes a request to the API and gets the data from the server then set the state and is rendered in the select tag in options
 getSuppliers = async ()=>{
     await axios.get('http://127.0.0.1:8000/suppliers').then( response =>{
     this.setState({ suppliers:response.data.body} , ()=>{ return this.state.suppliers})
     })
     }
-
-
-
-    getImages = async ()=>{
-        await axios.get('http://127.0.0.1:8000/products').then( response =>{
-    //    this.setState({image:response.data.body[0].imagep.data.toString('base64')} , ()=>{ return this.state.image});
-    this.setState({photo:response.data.photo[0].imagep} , ()=>{ return this.state.photo})
-})
-        }
-    
-
-
-handleChange = (event)=>{
-    // const file = event.target.files[0];
-    // this.setState({image:file} ,()=>{ return this.state.image})
-
-
-    const file = event.target.files[0];
-    this.setState({image:file} , ()=>{ return this.state.image});
-}
-
-
+//controls the form when is submitted
 submitHandler = (event)=>{
-
-
 event.preventDefault();
-const data = new FormData();
-data.append('productphoto' , this.state.image);
-axios.post('http://127.0.0.1:8000/products' , data).then( response =>{ console.log(response)}).catch(error =>{ console.log(error)});
+//this method creates a new form data and then we append all the information we want to send to tth server
+this.prepareToPost();
 }
 
+prepareToPost = async ()=>{
+    //create a variable of type FormData and the append the info with the field we are sending at the server and the value
+const product_photo = new FormData();
+
+product_photo.append('product_photo', this.state.product_photo);
+let { product_name } = this.state; 
+
+console.log(product_name)
+//make the POST request to the server
+//await axios.post('http://127.0.0.1:8000/products' , {}).then( (response)=>{ console.log(response.data)}).catch( (error)=>{ console.log(error)})
+}
 
 componentDidMount(){
-  //  this.getCategories();
-    //this.getSuppliers();
-    this.getImages();
+   this.getCategories();
+    this.getSuppliers();
+    //this.getImages();
 }    
 
-
-
-
-
 render(){
-    console.log(this.state.photo)
 return(
     <div>
         <h1>Add a Product to your inventory</h1>
        <form onSubmit={ this.submitHandler }>
            
-    
+       <div>
+           <label htmlFor="product_name">Product Name:  </label>
+           <input type="text" name="product_name" onChange={(event)=>{
+                this.setState({product_name:event.target.value},()=>{ return this.state.product_name})
+           }} />
+     </div>
+
+     <div>
+           <label htmlFor="product_description">Product Description:  </label>
+           <input type="text" name="product_description" onChange={(event)=>{ 
+               this.setState({product_description:event.target.value},()=>{ return this.state.product_description})
+           }} />
+     </div>
+            
+     <div>
+           <label htmlFor="product_price">Product Price:  </label>
+           <input type="text" name="product_price" onChange={(event)=>{
+               this.setState({product_price:event.target.value},()=>{ return this.state.product_price})
+           }} />
+     </div>
+
+     <div>
+           <label htmlFor="product_unit">Product Unit:  </label>
+           <input type="text" name="product_unit" onChange={(event)=>{ 
+               this.setState({ product_unit:event.target.value } , ()=>{ return this.state.product_unit })
+             }} />
+     </div>
+           
+     <div>
+           <label htmlFor="product_quantity">Product Quantity:  </label>
+           <input type="text" name="product_quantity" onChange={(event)=>{  
+               this.setState({product_quantity:event.target.value} , ()=>{ return this.state.product_quantity })
+           }} />
+     </div>
+               
            <div>
-           <label htmlFor="productphoto">Product photo:  </label>
-           <input type="file" name="productphoto" onChange={this.handleChange} />
+           <label htmlFor="product_photo">Product photo:  </label>
+           <input type="file" name="product_photo" onChange={(event)=>{
+               this.setState({product_photo:event.target.files[0]})
+           }} />
            </div>
 
+
+           <div>
+           <label htmlFor="product_category">Product Category:  </label>
+           <select name="product_category">
+           {
+               this.state.categories.map( category =>{ 
+               return(
+                  <option key={category.id_category} value={ category.id_category} onClick={ ()=>{
+                      this.setState({product_category: category.id_category})
+                  }} >{category.categoryName}</option>
+                      )
+                   })
+           }   
+           </select>
+           </div>
+
+           <div>
+           <label htmlFor="product_supplier">Product Supplier:  </label>
+           <select name="product_supplier">
+           {
+               this.state.suppliers.map( supplier =>{ 
+               return(
+                  <option key={ supplier.id_supplier } value={ supplier.id_supplier } onClick={()=>{ 
+                      this.setState({product_supplier: supplier.id_supplier } , ()=>{ return this.state.product_supplier})
+                  }}>{supplier.supplierName}</option>
+              )
+            })
+           }   
+           </select>
+           </div>
+
+           
+             <div>
              <button  type="submit">Submit Product</button>
+             </div>
 
            </form>
 
-
-     <img src={`data:image/jpg;base64,${this.state.photo}`} height="240px" />
 
 
     </div>
